5596ff184f0e2472f1aabb99119e09e8cf7afd2a
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-25 15:41:34 -0700

    sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * bubble_sort - function that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ *
+ * @array: given array to sort
+ * @size: size of the given array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp, swapped;
+	size_t i, j;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		swapped = 0;
+		for (i = 0; i < size - 1 - j; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sorts a doubly
+ * linked list of integers in ascending order using the
+ * Insertion sort algorithm
+ *
+ * @list: linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *cur, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (cur = (*list)->next; cur; cur = cur->next)
+	{
+		tmp = cur;
+		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+
+			if (tmp->prev != NULL)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+	}
+}
